8b8d8a1e1d11b79d16fd86672708c441c9f04772
Schoppidx <claas.schoppenhauer@gmx.ch>
2018-02-20 09:16:52 +0100

    man lad die scheisse hoch

---
+++ app/src/main/java/com/example/schoppenhauer/shoppinglisthq/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.schoppenhauer.shoppinglisthq;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
